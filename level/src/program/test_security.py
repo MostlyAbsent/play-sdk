c524a16c6499da1604b7bfdf489aa62b70c64666
J <j.doran.admin@gmail.com>
2025-10-04 16:50:21 +0800

    feat: refactor security test to better match the API
    Both from a security point of view and educational one.

--- level/src/program/test_security.py
+++ level/src/program/test_security.py
@@ -7,20 +7,39 @@ import json
 class ProgramSecurityTestCase(TestCase):
     fixtures = ['data.json']
 
-    def test_alice_password_ittr_w_firstName(self):
-        payload = json.dumps({"manager__user__password__startswith": "p",
-                              "firstName": "Robert"})
+    def test_api_reject_secure_property_query_manager(self):
+        """
+        The Employee.manager property should be protected, any query
+        containing manager references should be rejected
+        """
+        payload = json.dumps({"manager__firstName__startswith": "A"})
         res = self.client.post("/employee/",
                                data=payload,
                                content_type="application/json")
-        self.assertEqual(res.status_code, 200)
-        expected = """[{"firstName":"Robert","lastName":"Smith"}]"""
-        self.assertEqual(res.content.decode('utf-8'), expected)
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(res.content, [])
 
-    def test_alice_password_itterable(self):
-        payload = json.dumps({"manager__user__password__startswith": "p"})
+    def test_api_reject_secure_property_query_user(self):
+        """
+        The Employee.user property should be protected, any query
+        containing user references should be rejected
+        """
+        payload = json.dumps({"user__username__startswith": "A"})
         res = self.client.post("/employee/",
                                data=payload,
                                content_type="application/json")
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(json.loads(res.content), [])
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(res.content, [])
+
+    def test_api_reject_traversal_query_user(self):
+        """
+        The Employee.user property should be protected, any query
+        containing user references should be rejected, including those
+        that traverse a database relationship.
+        """
+        payload = json.dumps({"manager__user__username__startswith": "A"})
+        res = self.client.post("/employee/",
+                               data=payload,
+                               content_type="application/json")
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(res.content, [])
