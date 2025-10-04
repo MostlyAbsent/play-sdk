699551b31e59d5b18e775f60323ff0cbc8f36fa3
J <j.doran.admin@gmail.com>
2025-10-04 10:47:37 +0800

    feat: cleanup unneeded dirs

--- lib/php/src/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    // To pass uncomment the line below
-    //return 'Hello SecDim';
-    return 'Hello ';
-});
--- lib/python/src/program/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # /program/
-    path('', views.index, name='index'),
-    # /program/1
-    path('isoptimal/', views.isoptimal, name='isoptimal'),
-]
